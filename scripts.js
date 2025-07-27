64c5dcc2b74b2a36f36ce7d9f0c47901fd9719a1
Ramon <ramonempresa2001@gmail.com>
2025-07-27 18:59:26 -0300

    feat: Add number indicator update for carousel

--- scripts.js
+++ scripts.js
@@ -36,6 +36,8 @@ function update(direction) {
 
   items[active].classList.add("active");
   dots[active].classList.add("active");
+
+  numberIndicator.textContent = String(active + 1).padStart(2, '0')
 }
 
 // ------- esse SetInterval é o que faz o carrossel girar sozinho e o CleaInterval é o que limpa o temporizado e faz ficar no loop. 
