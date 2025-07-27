677aca600f72060cec6569fd23095f4ba3f697fd
Ramon <ramonempresa2001@gmail.com>
2025-07-27 18:47:08 -0300

    feat: Add update function for carousel navigation

--- scripts.js
+++ scripts.js
@@ -13,13 +13,36 @@ let active = 0
 const total = items.length
 let timer
 
+function update(direction) {
+    document.querySelector('.item.active').classList.remove('active')
+    
+    document.querySelector('.dot.active').classList.remove('active')
+
+    if (direction > 0) {
+        active = active + 1
+
+        if (active === total) {
+            active = 0
+        }
+
+    } else if (direction < 0) {
+        active = active -1
+
+        if (active < 0) {
+            active = total -1 
+        }
+    }
+
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+}
 
 prevButton.addEventListener('click', function() {
-    // update()
+    update(-1)
     
 })
 
 nextButton.addEventListener('click', function() {
-    // update()
+    update(1)
     
 })
